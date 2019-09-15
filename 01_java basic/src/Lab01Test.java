451763bb2942735bcff5375853f506ddcc4ace9e
progquartz <progquartz@gmail.com>
2019-09-15 22:17:38 +0900

    lab 01 & 02 tested

---
+++ 01_java basic/src/Lab01Test.java
@@ -0,0 +1,12 @@
+
+
+public class Lab01Test {
+
+    public static void main(String[] args)
+    {
+        Lab01 testLab = new Lab01();
+        testLab.infoCircle();
+        testLab.bmiCalculator();
+        testLab.wpCalculator();
+    }
+}
---
+++ Lab02/src/Examples02Test.java
@@ -0,0 +1,20 @@
+
+public class Examples02Test {
+
+	public static void main(String[] args) {
+		Examples02 ex02= new Examples02();
+		
+		ex02.PrimitiveTypesMinMax();
+       // ex02.drawDiamond();
+       // ex02.MysterySeries();
+       // System.out.println();
+      //  ex02.TwoLargest();
+      //  ex02.NonPrimeList();
+      //  ex02.OddEvenSum();
+      //  ex02.PrimeList();
+        
+
+
+	}
+
+}
